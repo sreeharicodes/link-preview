070aef6b9e5dc76da8077db0db12c94c59975f48
sreehari1997 <sreeharivijayan619@gmail.com>
2021-09-21 12:07:08 +0530

    init

---
+++ link-preview/preview.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_soup(url):
+    # https://andrejgajdos.com/how-to-hire-a-freelance-front-end-developer/
+    source = requests.get(url)
+    source.raise_for_status()
+    soup = BeautifulSoup(source.content, "html.parser")
+    return soup
+
+def get_title(soup):
+    # Open graph protocol
+    og_title = soup.find("meta", property="og:title")
+    if og_title:
+        return og_title.get('content')
+    # Twitter card
+    twitter_card = soup.find("meta", property="twitter:title")
+    if twitter_card:
+        return twitter_card.get('content')
+    # Title of the webpage
+    page_title = soup.title
+    if page_title:
+        return page_title.string
+    # Check for h1 tags
+    h1_title = soup.find("h1")
+    if h1_title:
+        return h1_title.string
+    # Check for h2 tags
+    h2_title = soup.find("h2")
+    if h2_title:
+        return h2_title.string
+
+    return None
